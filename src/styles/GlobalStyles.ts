903c89ced478265cb8125bca7a613a0845faeeb0
kila <luka.kilasonia.1@btu.edu.ge>
2024-01-31 02:34:53 +0400

    create global styles

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        width: 100vw;
+        height: 100vh;
+        background-color: rgb(15 23 42 / 1);
+    }
+`;
