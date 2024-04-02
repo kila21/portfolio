476945e5e0a8e66f693274242e117d83505f3719
kila <luka.kilasonia.1@btu.edu.ge>
2024-04-02 14:22:56 +0400

    resize func and display nav

--- src/components/Header/HeaderNav.tsx
+++ src/components/Header/HeaderNav.tsx
@@ -1,45 +1,56 @@
 import styled from "styled-components";
+import { HeaderNavItem } from "./HeaderNavItem";
+import { useEffect, useState } from "react";
 
 export const HeaderNav = () => {
+  const [activeNum, setActiveNum] = useState(0);
+  const [screen, setScreen] = useState(window.innerWidth);
+
+  const checkScreenSize = () => {
+    setScreen(window.innerWidth);
+    console.log(screen);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", checkScreenSize);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("resize", checkScreenSize);
+    };
+  }, []);
+
   return (
     <HeaderNavContainerStyled>
-      <HeaderNavigation>
-        <HeaderNavItem>
-          <HeaderNavItemLink>
-            <HeaderNavItemIndicator></HeaderNavItemIndicator>
-            <HeaderNavItemtext>about</HeaderNavItemtext>
-          </HeaderNavItemLink>
-        </HeaderNavItem>
-      </HeaderNavigation>
+      {screen >= 1024 && (
+        <HeaderNavigation>
+          <HeaderNavItem
+            title="about"
+            active={activeNum === 0 && true}
+            click={() => setActiveNum(0)}
+          />
+          <HeaderNavItem
+            title="experience"
+            active={activeNum === 1 && true}
+            click={() => setActiveNum(1)}
+          />
+          <HeaderNavItem
+            title="projects"
+            active={activeNum === 2 && true}
+            click={() => setActiveNum(2)}
+          />
+        </HeaderNavigation>
+      )}
     </HeaderNavContainerStyled>
   );
 };
 
 const HeaderNavContainerStyled = styled.div`
-  width: 100%;
+  width: 90%;
+  margin-top: 50px;
 `;
 
 const HeaderNavigation = styled.nav`
   display: flex;
   flex-direction: column;
-`;
-
-const HeaderNavItem = styled.li`
-  list-style: none;
-`;
-
-const HeaderNavItemLink = styled.a`
-  outline: none;
-  text-align: center;
-`;
-
-const HeaderNavItemIndicator = styled.span``;
-
-const HeaderNavItemtext = styled.span`
-  color: ${(props) => props.theme.colors.grey};
-  text-transform: uppercase;
-
-  &:hover {
-    color: ${(props) => props.theme.colors.white};
-  }
 `;
