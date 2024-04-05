4f292d12b11055ab68ff61e2c8af5967459f0d49
kila <luka.kilasonia.1@btu.edu.ge>
2024-04-05 14:32:01 +0400

    update experiences and display

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -5,12 +5,32 @@ export const Experience = () => {
   return (
     <ExperienceStyled id="experience">
       <ExperienceTitle>Experience </ExperienceTitle>
-      <ExperienceItem></ExperienceItem>
+      <ExperienceItem
+        date="2022 - present"
+        title="React Developer(online Courses)"
+        text="After working as an Angular Developer, I start to develope myself and learn React. I took Udemy React Course. watching some videos in youtube and also try to use google and ChatGPT. Currently, trying to Build some project, like portfolio and find Job where i took some experience and work to develope my Rect Skills."
+        tools={["React, Redux", "Router", "Styled Components"]}
+      />
+
+      <ExperienceItem
+        date="July - Dec, 2021"
+        title="TBC Angular Course"
+        text="6 month, TBC Angular Course, Where I Learn A lot about Angular, Typescript and Git. Working with group of people And myself to build some project And learn more a about Coding and proccess of working real projects."
+        tools={["Typescript", "Angular", "Git", "HTML", "CSS", "SCSS"]}
+      />
+
+      <ExperienceItem
+        date="Apr - Sep, 2022"
+        title="Junior Angular Developer"
+        text="After A course, I Start A job As a Freelance Junior Angular Developer. Where, I Learn A state Managment for Angular(ngrx and rxjs).And my job was to rewrite website and implement some new designs.Like re-branding"
+        tools={["Typescript", "Angular", "Git", "Ngrx", "Rxjs"]}
+      />
     </ExperienceStyled>
   );
 };
 
 const ExperienceStyled = styled.div`
+  cursor: pointer;
   display: flex;
   flex-direction: column;
   margin: 50px;
--- src/components/Experience/ExperienceItem.tsx
+++ src/components/Experience/ExperienceItem.tsx
@@ -1,20 +1,18 @@
 import styled from "styled-components";
 
-export const ExperienceItem = () => {
+export const ExperienceItem = (props: {
+  date: string;
+  title: string;
+  text: string;
+  tools: string[];
+}) => {
   return (
     <ExperienceItemStyled>
-      <ExperienceItemDate>2014 - 2020</ExperienceItemDate>
+      <ExperienceItemDate>{props.date}</ExperienceItemDate>
       <ExperienceItemContent>
-        <ExperienceItemTitle>
-          Senior Frontend Engineer, Accessibility ·
-        </ExperienceItemTitle>
+        <ExperienceItemTitle>{props.title}</ExperienceItemTitle>
 
-        <ExperienceItemText>
-          Build and maintain critical components used to construct Klaviyo’s
-          frontend, across the whole product. Work closely with cross-functional
-          teams, including developers, designers, and product managers, to
-          implement and advocate for best practices in web accessibility.
-        </ExperienceItemText>
+        <ExperienceItemText>{props.text}</ExperienceItemText>
       </ExperienceItemContent>
     </ExperienceItemStyled>
   );
@@ -23,8 +21,17 @@ export const ExperienceItem = () => {
 const ExperienceItemStyled = styled.div`
   display: flex;
   margin-top: 50px;
+  min-height: 200px;
+
   @media (max-width: 768px) {
     flex-direction: column;
+  }
+
+  @media (min-width: 1024px) {
+    &:hover {
+      background-color: rgba(30, 41, 59, 0.5);
+      border-radius: 10px;
+    }
   }
 `;
 
@@ -32,6 +39,7 @@ const ExperienceItemDate = styled.div`
   color: ${(props) => props.theme.colors.grey};
   width: 35%;
   margin-right: 20px;
+  padding: 10px 10px;
   @media (max-width: 768px) {
     width: 100%;
     margin-bottom: 15px;
@@ -41,6 +49,7 @@ const ExperienceItemDate = styled.div`
 const ExperienceItemContent = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: space-around;
 `;
 
 const ExperienceItemTitle = styled.h4`
@@ -49,5 +58,7 @@ const ExperienceItemTitle = styled.h4`
 
 const ExperienceItemText = styled.p`
   color: ${(props) => props.theme.colors.grey};
+  line-height: 1.7;
   margin-top: 15px;
+  margin-bottom: 20px;
 `;
