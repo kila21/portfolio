8e4079b94bf198b323586753b03ff60ecd1e0c66
kila <luka.kilasonia.1@btu.edu.ge>
2024-04-08 15:09:33 +0400

    update app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import { About } from "./components/About/About";
+import { Experience } from "./components/Experience/Experience";
+import { Header } from "./components/Header/Header";
+import styled from "styled-components";
+
 function App() {
-  return <>Hello Portfolio</>;
+  return (
+    <>
+      <Header />
+      <MainContentStyled>
+        <About />
+        <Experience />
+      </MainContentStyled>
+    </>
+  );
 }
 
 export default App;
+
+const MainContentStyled = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  @media (max-width: 1024px) {
+    width: 100%;
+  }
+`;
