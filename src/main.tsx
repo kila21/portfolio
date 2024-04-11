e61b42f6cc48b27ad61550cbe7bdb2e7fcafcaa6
kila <luka.kilasonia.1@btu.edu.ge>
2024-04-11 16:38:17 +0400

    add router

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,22 @@ import App from "./App.tsx";
 import { GlobalStyles } from "./styles/GlobalStyles.ts";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./styles/theme.ts";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { FullProjectsPage } from "./components/FullProjectsPage/FullProjectsPage.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  { path: "/projects", element: <FullProjectsPage /> },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
       <GlobalStyles />
-      <App />
     </ThemeProvider>
   </React.StrictMode>
 );
